1896b79c4543426ba6aec63861b50f5d79cc8c0c
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2023-07-26 22:48:17 +0600

    Added 6 questions

--- app.js
+++ app.js
@@ -198,3 +198,35 @@ Example output: "elephant"
 
 // console.log(emojifyPhrase("I :heart: my :cat:"));
 // console.log(emojifyPhrase("I :heart: my :elephant:"));
+
+// Question 6 6️⃣
+
+/* 
+
+Anagrams are groups of words that can be spelled with the same letters. 
+For example, the letters in "pea" can be rearrange to spell "ape", and 
+the letters in "allergy" can be rearranged to spell "gallery."
+
+Write a function to check if two strings of lowercase letters are anagrams. 
+Return true if the word is an anagram. Return false if it isn't. 
+
+Example input: "allergy", "gallery"
+Example output: true
+
+Example input: "rainbow", "crossbow"
+Example output: false
+
+*/
+
+// function isAnagram(str1, str2){
+//     if(str1.length !== str2.length) return false
+
+//     const newStr1 = str1.split(' ').sort().join('')
+
+//     const newStr2 = str2.split(' ').sort().join('')
+
+//     newStr1 === newStr2 ? true : false
+// }
+
+// console.log(isAnagram("allergy", "gallery"));
+// console.log(isAnagram("treasure", "measure"));
