2c84bb4f5ff85bac7f1b9ff97240ab0bedf771e8
Bibarta Kumar Sarkar <bibartaks@gmail.com>
2023-07-26 21:52:40 +0600

    Added 1st question

---
+++ app.js
@@ -0,0 +1,19 @@
+// Question 1 1️⃣
+// Example input: "Hello"
+// Example output: "HELLO!"
+
+// Example input: "I'm almost out of coffee"
+// Example output: "I'M 😱 ALMOST 😱 OUT 😱 OF 😱 COFFEE!"
+
+// .split() .join()
+// */
+// function panic(str){
+//     return str
+//         .split(' ')
+//         .join(' 😱 ')
+//         .toUpperCase() + "!";
+// }
+
+// // Test your function
+// console.log(panic("I'm almost out of coffee"));
+// console.log(panic("winter is coming"))
